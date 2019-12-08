b0932932618a1f6c5a27b71ffd5343c2bfff27cd
YSpektor <spektor.ym@gmail.com>
2019-12-08 20:15:10 +0300

    Implemented dxf circle and ellipse

--- examples/basic-dxf/index.js
+++ examples/basic-dxf/index.js
@@ -3,4 +3,6 @@ const fs = require('fs');
 
 const dxf = new DxfDrawing(200, 100);
 dxf.line(10, 10, 10, 90, {stroke: 'red', 'stroke-dasharray': '2 1'});
+dxf.circle(50, 50, 30, {fill: 'cyan'});
+dxf.ellipse(120, 50, 30, 20, {stroke: '#0f0'});
 fs.writeFileSync(__dirname + '/example.dxf', dxf.dxf());
--- src/dxf/dxf-builder.ts
+++ src/dxf/dxf-builder.ts
@@ -1,7 +1,8 @@
 import { DxfDocument } from 'dxf-doc';
-import { Line } from 'dxf-doc/dist/entities';
+import { Line, Entity, Hatch, Circle, Ellipse } from 'dxf-doc/entities';
 
 import { Attributes, Point, Alignment, IRegion, IPath } from '..';
+import { EdgeBoundaryPath, HatchPattern } from 'dxf-doc/entities/hatch';
 
 interface Style {
     stroke?: string;
@@ -26,6 +27,7 @@ const COLORS = [
 
 export abstract class AbstractDxfBuilder {
     private ltypes = new Set<string>();
+    private patterns = new Map<string, HatchPattern>();
 
     constructor(protected document: DxfDocument) { }
 
@@ -38,6 +40,7 @@ export abstract class AbstractDxfBuilder {
                 Array.isArray(attributes['stroke-dasharray']) ? (attributes['stroke-dasharray'] as (number | string)[]).map(x => +x) : 
                 typeof attributes['stroke-dasharray'] === 'string' ? (attributes['stroke-dasharray'] + '').split(' ').map(x => +x) : undefined
                 : undefined;
+            style.fill = attributes.fill;
         }
         return style;
     }
@@ -63,27 +66,81 @@ export abstract class AbstractDxfBuilder {
         return colorIndex >= 0 ? colorIndex : undefined;
     }
 
+    private getFillPattern(style: Style): string | undefined {
+        let pattern;
+        if (style.fill) {
+            const match = style.fill.match(/^url\(#(.+)\)$/);
+            if (match) {
+                pattern = match[1];
+            }
+        }
+        return pattern;
+    }
+
     protected abstract convertY(y: number): number;
 
     line(x1: number, y1: number, x2: number, y2: number, attributes?: Attributes): void {
         const style = this.getStyle(attributes);
-        const entity = new Line(this.document, x1, this.convertY(y1), x2, this.convertY(y2));
-        entity.ltype = this.getLType(style);
-        entity.lineWeight = this.getLineWeight(style);
-        entity.color = this.getColor(style, 'stroke');
-        this.document.addEntity(entity);
+        if (style.stroke !== 'none') {
+            const entity = new Line(this.document, x1, this.convertY(y1), x2, this.convertY(y2));
+            entity.ltype = this.getLType(style);
+            entity.lineWeight = this.getLineWeight(style);
+            entity.color = this.getColor(style, 'stroke');
+            this.document.addEntity(entity);
+        }
     }
 
     rect(x: number, y: number, width: number, height: number, attributes?: Attributes | undefined): void {
-        throw new Error("Method not implemented.");
+        this.polygon([{x, y}, {x: x + width, y}, {x: x + width, y: y + height}, {x, y: y + height}]);
     }
 
     circle(cx: number, cy: number, r: number, attributes?: Attributes | undefined): void {
-        throw new Error("Method not implemented.");
+        const style = this.getStyle(attributes);
+        if (style.fill) {
+            const patternName = this.getFillPattern(style);
+            const boundary = new EdgeBoundaryPath();
+            boundary.acr(cx, this.convertY(cy), r, 0, 360, true);
+            const pattern = patternName ? this.patterns.get(patternName) : undefined;
+            const entity = new Hatch(this.document, [boundary], pattern);
+            entity.color = this.getColor(style, 'fill');
+            this.document.addEntity(entity);
+        }
+        if (style.stroke !== 'none') {
+            const entity = new Circle(this.document, cx, this.convertY(cy), r);
+            entity.ltype = this.getLType(style);
+            entity.lineWeight = this.getLineWeight(style);
+            entity.color = this.getColor(style, 'stroke');
+            this.document.addEntity(entity);
+        }
     }
 
     ellipse(cx: number, cy: number, rx: number, ry: number, attributes?: Attributes | undefined): void {
-        throw new Error("Method not implemented.");
+        const style = this.getStyle(attributes);
+        if (style.fill) {
+            const patternName = this.getFillPattern(style);
+            const boundary = new EdgeBoundaryPath();
+            if (rx > ry) {
+                boundary.ellipse(cx, this.convertY(cy), rx, 0, ry / rx * 100, 0, 360, true);
+            } else {
+                boundary.ellipse(cx, this.convertY(cy), 0, ry, rx / ry * 100, 0, 360, true);
+            }
+            const pattern = patternName ? this.patterns.get(patternName) : undefined;
+            const entity = new Hatch(this.document, [boundary], pattern);
+            entity.color = this.getColor(style, 'fill');
+            this.document.addEntity(entity);
+        }
+        if (style.stroke !== 'none') {
+            let entity: Ellipse;
+            if (rx > ry) {
+                entity = new Ellipse(this.document, cx, this.convertY(cy), rx, 0, ry / rx);
+            } else {
+                entity = new Ellipse(this.document, cx, this.convertY(cy), 0, ry, rx / ry);
+            }
+            entity.ltype = this.getLType(style);
+            entity.lineWeight = this.getLineWeight(style);
+            entity.color = this.getColor(style, 'stroke');
+            this.document.addEntity(entity);
+        }
     }
 
     polyline(pts: Point[], attributes?: Attributes | undefined): void {
@@ -93,6 +150,7 @@ export abstract class AbstractDxfBuilder {
     polygon(pts: Point[], attributes?: Attributes | undefined): void {
         throw new Error("Method not implemented.");
     }
+    
     polybezier(pts: Point[], attributes?: Attributes | undefined): void {
         throw new Error("Method not implemented.");
     }
@@ -119,5 +177,9 @@ export abstract class AbstractDxfBuilder {
     
     createPath(): IPath {
         throw new Error("Method not implemented.");
+    }
+
+    addPattern(pattern: HatchPattern) {
+        this.patterns.set(pattern.name, pattern);
     }
 }
